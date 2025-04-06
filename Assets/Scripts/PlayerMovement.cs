d0abea913293bcb123fabdc4bf5b4bf78fae8205
LukeLynn <83674585+LukeLynn@users.noreply.github.com>
2025-04-06 20:02:41 +0100

    PlayerController Finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,21 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D body;
 
-    public float speed;
-    public float drag;
+    public float groundSpeed;
+    public float jumpSpeed;
+    public float acceleration;
+
+    [Range(0f, 1f)]
+
+    public float groundDecay;
 
     public bool grounded;
 
-    public b
+    public BoxCollider2D groundCheck;
+    public LayerMask groundMask;
+
+    float xInput;
+    float yInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,43 +32,62 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
-
-        if (Mathf.Abs(xInput) > 0)
-        {
-            body.linearVelocity = new Vector2(xInput * speed, body.linearVelocity.y);
-        }
-
-        if (Mathf.Abs(yInput) > 0)
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, yInput * speed);
-        }
-
-
+        GetInput();
+        HandleJump();
         
+
+
         //Vector2 direction = new Vector2(xInput, yInput).normalized;
         //body.linearVelocity = direction * speed;
     }
 
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         CheckGround();
+        MoveWithInput();
+        ApplyFriction();
+    }
+    void GetInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
 
-        if (grounded)
+    void MoveWithInput()
+    {
+
+        if (Mathf.Abs(xInput) > 0)
         {
-            body.linearVelocity *= drag;
+            float increment = xInput * acceleration;
+            float newSpeed = Mathf.Clamp(body.linearVelocityX + increment, -groundSpeed, groundSpeed);
+
+            body.linearVelocity = new Vector2(newSpeed, body.linearVelocityY);
+            float direction = Mathf.Sign(xInput);
+            transform.localScale = new Vector3(direction, 1, 1);
+        }
+
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocityX, jumpSpeed);
         }
     }
 
-
     void CheckGround()
     {
-
+        grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
     }
 
-
+    void ApplyFriction()
+    {
+        if (grounded && xInput == 0 && body.linearVelocityY == 0)
+        {
+            body.linearVelocity *= groundDecay;
+        }
+    }
 
 
 
