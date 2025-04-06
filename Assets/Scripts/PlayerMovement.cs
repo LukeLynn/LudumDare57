4bd69d48d847aafddd06757ddec0f84038ee9ac9
LukeLynn <83674585+LukeLynn@users.noreply.github.com>
2025-04-06 01:10:16 +0100

    PlayerMovement V1.0
    Topdown player movement in a working state. Now needing to develop into a platformer style of movement.
    
    Collision is working with the player and the platforms.
    
    Added this commit:
    - Player movement script
    - Geometry added with collision
    - Speed and Gravity variables (Need updating to feel more fluid)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEditor.Rendering;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D body;
+
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(xInput) > 0)
+        {
+            body.linearVelocity = new Vector2(xInput * speed, body.linearVelocity.y);
+        }
+
+        if (Mathf.Abs(yInput) > 0)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, yInput * speed);
+        }
+
+
+        
+        //Vector2 direction = new Vector2(xInput, yInput).normalized;
+        //body.linearVelocity = direction * speed;
+    }
+}
